736d90425652706e7de1c0128fc50bf6e2d81ae3
Subhodip1307 <sardarsubhodip857@gmail.com>
2026-06-29 01:57:22 +0530

    Now config can be written in a differnt file

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,34 @@ use axum::{
     http::StatusCode,
     routing::{get, post},
 };
-use std::sync::LazyLock;
+use std::process::Command;
 use std::{collections::HashMap, net::SocketAddr};
 use tokio::sync::mpsc;
 mod structures;
 use sha2::{Digest, Sha256};
-use structures::{Bashinfo, DockerPayload, Token};
+use structures::{Config, Bashinfo,DockerPayload, Token};
 use tokio::fs;
-static PROJECTS: LazyLock<HashMap<&str, Bashinfo>> = LazyLock::new(|| {
-    HashMap::from([(
-        "",
-        Bashinfo {
-            path: "",
-            shahash: "",
-            project: "",
-        },
-    )])
-});
-use std::process::Command;
-const DEV: &str = "";
-const ORG: &str = "";
-const TAG: &str = "";
+use tokio::sync::OnceCell;
+
+static PROJECTS: OnceCell<HashMap<String, Bashinfo>> =OnceCell::const_new();
+static DEV: OnceCell<String> =OnceCell::const_new();
+static ORG: OnceCell<String> =OnceCell::const_new();
+static TAG: OnceCell<String> =OnceCell::const_new();
+
 
 #[tokio::main]
 async fn main() {
+    println!("Runing Version {}", env!("CARGO_PKG_VERSION"));
+    // setting values to the variables
+    set_values().await;
+    println!("new hasmpa is {:?}",PROJECTS.get());
     let (tx, rx) = mpsc::channel::<String>(10);
     tokio::spawn(async move {
         worker(rx).await;
     });
     let app = Router::new()
         .route("/", get(|| async { "runing" }))
-        .route("/send", post(docker_view))
+        .route(&format!("/{}",option_env!("url_path").unwrap_or("send")), post(docker_view))
         .with_state(tx);
     let addr: SocketAddr = "0.0.0.0:8000".parse().unwrap();
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
@@ -47,10 +44,15 @@ async fn docker_view(
     Json(data): Json<DockerPayload>,
 ) -> StatusCode {
     println!("data is {:?}", data);
+    let  __project= match PROJECTS.get(){
+        Some(v)=>v,
+        None=>&HashMap::new(),
+    };
+    
 
     if data.is_deployble()
-        && PROJECTS.contains_key(&params.token.as_str())
-        && data.check_repo_name(PROJECTS[&params.token.as_str()].project)
+        && __project.contains_key(&params.token)
+        && data.check_repo_name(__project[&params.token].get_repo())
     {
         match sender.send(params.token).await {
             Ok(_) => return StatusCode::CREATED,
@@ -61,9 +63,15 @@ async fn docker_view(
 }
 
 async fn worker(mut rece: mpsc::Receiver<String>) {
+    let  __project= match PROJECTS.get(){
+        Some(v)=>v,
+        None=>&HashMap::new(),
+    };
+
     while let Some(t) = rece.recv().await {
-        let item = &PROJECTS[t.as_str()];
-        if let Ok(content) = fs::read(item.path).await {
+        let item = &__project[t.as_str()];
+        if let Ok(content) = fs::read(item.get_path()).await {
+            // TODO: check the file size before genating hash
             let hex = hex::encode(Sha256::digest(&content));
             if hex != item.shahash {
                 println!("hash Matching failed");
@@ -71,7 +79,7 @@ async fn worker(mut rece: mpsc::Receiver<String>) {
             }
             // executing the bash
             let output = Command::new("bash")
-                .arg(item.path) // path to your .sh file
+                .arg(item.get_path()) // path to your .sh file
                 .output() // runs it, waits, captures stdout/stderr
                 .expect("failed to run script");
             println!("status: {}", output.status);
@@ -79,4 +87,35 @@ async fn worker(mut rece: mpsc::Receiver<String>) {
             println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
         };
     }
+}
+
+async fn set_values(){
+    #[cfg(debug_assertions)]
+    let read_file=tokio::fs::read_to_string("config.toml").await;
+    #[cfg(not(debug_assertions))]
+    let read_file=tokio::fs::read_to_string("/etc/deploy_bridge/config.toml").await;
+    
+    let file_data= match read_file {
+        Ok(v)=>v,
+        Err(e)=>{ println!("erro is {}",e);return },
+    };
+
+    let load_config:Result<Config, toml::de::Error>=toml::from_str(&file_data);
+
+    match load_config {
+       Err(e)=>{ println!("erro 2is {}",e);return },
+        Ok(v)=>{
+            println!("setting value");
+            let _=DEV.set(v.main.dev);
+            let _=ORG.set(v.main.org);
+            let _=TAG.set(v.main.tag);
+            // now setting hashmap 
+            let e=PROJECTS.set(v.projects);
+            match e {
+                Err(err)=>println!("err is {:?}",err),
+                _=>{}
+            }
+        }
+    }
+
 }
--- src/structures.rs
+++ src/structures.rs
@@ -1,4 +1,6 @@
 use crate::{DEV, ORG, TAG};
+use serde::Deserialize;
+use std::collections::HashMap;
 
 #[derive(serde::Deserialize)]
 pub struct Token {
@@ -26,19 +28,44 @@ pub struct DockerPayload {
 }
 impl DockerPayload {
     pub fn is_deployble(&self) -> bool {
-        self.push_data.tag == TAG
-            && self.push_data.pusher == DEV
+        let org=ORG.get().unwrap();
+        &self.push_data.tag == TAG.get().unwrap()
+            && &self.push_data.pusher == DEV.get().unwrap()
             && self.repository.is_private
-            && self.repository.namespace == ORG
-            && self.repository.repo_name.starts_with(ORG)
+            && &self.repository.namespace == org
+            && self.repository.repo_name.starts_with(org)
     }
     pub fn check_repo_name(&self, name: &str) -> bool {
         self.repository.name == name
     }
 }
 
-pub struct Bashinfo<'a> {
-    pub project: &'a str,
-    pub path: &'a str,
-    pub shahash: &'a str,
+#[derive(Debug, Deserialize)]
+pub struct Main {
+    #[serde(rename = "DEV")]
+    pub dev: String,
+    #[serde(rename = "ORG")]
+    pub org: String,
+    #[serde(rename = "TAG")]
+    pub tag: String,
+}
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub main: Main,
+    #[serde(rename="R")]
+    pub projects: HashMap<String, Bashinfo>,
+}
+#[derive(Debug, Deserialize)]
+pub struct Bashinfo {
+    pub repo:String,
+    pub path: String,
+    pub shahash: String,
+}
+impl Bashinfo {
+    pub fn get_repo(&self)->&str{
+        &self.repo
+    }
+    pub fn get_path(&self)->&str{
+        &self.path
+    }
 }
